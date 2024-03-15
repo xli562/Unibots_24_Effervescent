61130281c8dc28821b5a7cdd904735c6c4363b7f
Xiaoyang Li <xl562@cam.ac.uk>
2024-03-15 16:00:08 +0000

    updated devlog (LXY)

--- motor_drive_modules/Chassis.py
+++ motor_drive_modules/Chassis.py
@@ -59,6 +59,26 @@ class Motor:
         self.set_position(self.position)
 
 
+class Servo():
+    """ The class to represent the gripper's servo. """
+    
+    def __init__(self, port):
+        self._port = port
+    
+    def set_position(self, degrees):
+        """ Rotate the servo to specified angular position. """
+
+    def grip(self):
+        """ Grip a mini rugby. """
+        ''' TODO: Calibration '''
+        pass
+
+    def release(self):
+        """ Release a mini rugby. """
+        ''' TODO: Calibration '''
+        pass
+
+
 class MecanumDrive:
     def __init__(self):
         """Initializes the mecanum drive with four motors."""
