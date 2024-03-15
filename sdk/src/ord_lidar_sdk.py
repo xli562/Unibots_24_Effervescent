2152e9a7adb3a1bcad20bda69518f89addb409ff
Harry-QY <qy250@cam.ac.uk>
2024-03-15 13:59:37 +0000

    init

---
+++ sdk/src/ord_lidar_sdk.py
@@ -0,0 +1,29 @@
+import pybind11
+
+class lidar:
+    def __init__(self):
+        pass
+    def SetSerialPort(self):
+        pass
+    def Connect(self):
+        pass
+    def Disconnect(self):
+        pass
+    def isConnected(self):
+        pass
+    def Activate(self):
+        pass
+    def Deactivate(self):
+        pass
+    def GrabOneScan(self):
+        pass
+    def GrabOneScanBlocking(self):
+        pass
+    def GrabFullScan(self):
+        pass
+    def GrabFullScanBlocking(self):
+        pass
+    def GetTimestamp(self):
+        pass
+    def GetRotationSpeed(self):
+        pass
