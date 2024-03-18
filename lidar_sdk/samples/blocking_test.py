c3db91504fd90235e7fe899a5162e2ab94613ca1
Harry-QY <qy250@cam.ac.uk>
2024-03-18 13:42:52 +0000

    rewrote plot

--- lidar_sdk/samples/blocking_test.py
+++ lidar_sdk/samples/blocking_test.py
@@ -1,25 +1,22 @@
-import re
 import subprocess
 import threading
 import matplotlib.pyplot as plt
+import numpy as np
+import re
 import time
 
+# Initialize matplotlib in interactive mode
 plt.ion()
+
+# Setup the polar plot
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 r_data, theta_data = [], []
-line, = ax.plot(theta_data, r_data, 'b-')  # 'b-' sets the line color to blue
+line, = ax.plot(theta_data, r_data)
 
-
-# The path to the directory where you want to run the command
-working_directory = '/home/eff/Desktop/Unibots_24_Effervescent/lidar_sdk/build'
-
-# The command you want to run
-command = './blocking_test'  # Ensure this is executable in the target directory
-
-# Function to update the polar plot with new data
+# Function to update plot (called from the main thread)
 def update_plot(new_theta, new_r):
-    theta_data.append(new_theta)
     r_data.append(new_r)
+    theta_data.append(new_theta)
     line.set_xdata(theta_data)
     line.set_ydata(r_data)
     ax.relim()
@@ -27,37 +24,34 @@ def update_plot(new_theta, new_r):
     fig.canvas.draw()
     fig.canvas.flush_events()
 
-# Function to capture and plot output on a polar plot
-def capture_and_plot(pipe):
-    pattern = re.compile(r'\[(\d+): ([\d.]+), ([\d.]+)\]')
+# Thread function for capturing output
+def capture_output(pipe, data_queue):
+    pattern = re.compile(r'\[\d+: ([\d.]+), ([\d.]+)\]')
     for line in iter(pipe.readline, ''):
-        print(f"Data: {line}", end='')
         match = pattern.search(line)
         if match:
-            # No need for index in plotting, but it's parsed in case you need it
-            index = int(match.group(1))
-            new_r = float(match.group(2))  # Distance
-            new_theta = float(match.group(3))  # Angle, assuming it's already in radians
-            
-            # If your angle is in degrees, convert to radians with np.radians
-            # new_theta = np.radians(new_theta)
-            
-            update_plot(new_theta, new_r)
+            new_r = float(match.group(1))  # Radius (distance)
+            new_theta = np.radians(float(match.group(2)))  # Angle, converted to radians
+            data_queue.append((new_theta, new_r))  # Append tuple to queue
 
-# Start the process
+# Start the subprocess
+working_directory = '/path/to/your/directory'
+command = './your_command'
 process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True, cwd=working_directory, shell=True)
 
-# Create threads to handle stdout and stderr output
-stdout_thread = threading.Thread(target=capture_and_plot, args=(process.stdout,))
-
-# Start the threads
+# Data queue and thread setup
+data_queue = []
+stdout_thread = threading.Thread(target=capture_output, args=(process.stdout, data_queue))
 stdout_thread.start()
 
-# Main loop to keep the script alive
 try:
-    while process.poll() is None:
-        time.sleep(0.1)
+    while process.poll() is None or stdout_thread.is_alive():
+        # Process any new data in the queue
+        while data_queue:
+            new_theta, new_r = data_queue.pop(0)  # Get new data
+            update_plot(new_theta, new_r)  # Update the plot
+        time.sleep(0.1)  # Briefly sleep to yield control
 finally:
     stdout_thread.join()
-    plt.ioff()  # Turn off interactive mode
-    plt.show()  # Show the final plot
+    plt.ioff()
+    plt.show()
