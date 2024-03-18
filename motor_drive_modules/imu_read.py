4530f5f42414c796e7dd31e5b5a7a89d557564b0
MaxzzzLCX <cl927@cam.ac.uk>
2024-03-18 18:57:49 +0000

    (Max) IMU 003

--- motor_drive_modules/imu_read.py
+++ motor_drive_modules/imu_read.py
@@ -1,6 +1,28 @@
 from RosmasterBoard import Rosmaster
+import time
 
 bot = Rosmaster()
-while True:
+bot.create_receive_threading()
+
+print(time.time())
+
+def measure_stationary_yaw_drift_rate():
+    start = time.time()
     roll, pitch, yaw = bot.get_imu_attitude_data()
-    print(f"Roll: {roll}, Pitch: {pitch}, Yaw: {yaw}")
+    end = time.time()
+    
+    while end - start < 5:
+        end = time.time()
+        roll_end, pitch_end, yaw_end = bot.get_imu_attitude_data()
+    
+    yaw_rate = (yaw_end - yaw)/(end-start)
+    return yaw_rate
+
+print("Start Measure")
+yaw_rate = measure_stationary_yaw_drift_rate()
+print("Yaw Rate: {}".format(yaw_rate))
+
+
+# while True:
+#     roll, pitch, yaw = bot.get_imu_attitude_data()
+#     print(f"Roll: {roll}, Pitch: {pitch}, Yaw: {yaw}")
