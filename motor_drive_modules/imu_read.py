2471380288e3b13cd5b50ae4e9ac1a2b08a4fc88
MaxzzzLCX <cl927@cam.ac.uk>
2024-03-18 19:07:30 +0000

    (Max) IMU calibration test

--- motor_drive_modules/imu_read.py
+++ motor_drive_modules/imu_read.py
@@ -4,7 +4,9 @@ import time
 bot = Rosmaster()
 bot.create_receive_threading()
 
-print(time.time())
+imu_start = time.time()
+roll_0, pitch_0, yaw_0 = bot.get_imu_attitude_data()
+
 
 def measure_stationary_yaw_drift_rate():
     start = time.time()
@@ -18,11 +20,22 @@ def measure_stationary_yaw_drift_rate():
     yaw_rate = (yaw_end - yaw)/(end-start)
     return yaw_rate
 
+
+def get_imu_after_calibration(yaw_rate):
+    roll, pitch, yaw = bot.get_imu_attitude_data()
+    yaw = yaw - (time.time() - imu_start)*yaw_rate
+    return yaw
+
+
+
+
 print("Start Measure")
 yaw_rate = measure_stationary_yaw_drift_rate()
 print("Yaw Rate: {}".format(yaw_rate))
 
-
+while True:
+    real_yaw = get_imu_after_calibration(yaw_rate)
+    print("Yaw: {}".format(real_yaw))
 # while True:
 #     roll, pitch, yaw = bot.get_imu_attitude_data()
 #     print(f"Roll: {roll}, Pitch: {pitch}, Yaw: {yaw}")
