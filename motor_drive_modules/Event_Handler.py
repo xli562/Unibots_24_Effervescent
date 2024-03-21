08955fca3a304afa33333cb90c3eda192575952b
MaxzzzLCX <cl927@cam.ac.uk>
2024-03-21 19:15:54 +0000

    (Max) Update. Event_Handler & Main.py

--- motor_drive_modules/Event_Handler.py
+++ motor_drive_modules/Event_Handler.py
@@ -3,29 +3,36 @@ import time
 
 index_out = 0
 index_in = 0
-ser = serial.Serial("/dev/ttyACM0", 115200)
 
 class Event_Handler():
 
     def __init__(self):
-        self.reset = False
+        self.ser = serial.Serial("/dev/ttyACM0", 115200)
+        self.reset_flag = False
+        self.timeout_flag = False
+        
     
     def check_restart(self):
+        """
+        Check the Serial. If the message of "!Restart!" (sent from Arduino) is present, break from the main loop.
+        """
         print('restart_point_1')
         try:
-            if ser.in_waiting > 0:
-                data_str = ser.readline().strip().decode('utf-8')
+            if self.ser.in_waiting > 0:
+                data_str = self.ser.readline().strip().decode('utf-8')
                 print('Received Data: {}'.format(data_str))
                 readings = data_str.split('!')
                 for reading in readings:
                     if reading == "Restart":  # Command to restart - sent when restart button of Arduino is pressed
                         print('RESTARTING THE ROBOT')
-                        self.reset = True
+                        self.reset_flag = True
+                        break # CHECK IF NEED THIS
         except Exception as e:
             print(e)
 
     def empty_events(self):
-        self.reset = False
+        self.reset_flag = False
+
 
 event_handler = Event_Handler()
 
