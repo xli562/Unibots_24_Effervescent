010c1a1d549d78377c0bbee75911d3399018d793
Harry-QY <qy250@cam.ac.uk>
2024-03-22 16:32:04 +0000

    update chassis.py and pid_tuning.py

--- motor_drive_modules/pid_tuning.py
+++ motor_drive_modules/pid_tuning.py
@@ -1,6 +1,5 @@
 from RosmasterBoard import Rosmaster
-from ultrasound_via_arduino import Ultrasound
-from Chassis import Chassis
+from Chassis import *
 import time
 import serial
 
@@ -11,23 +10,28 @@ bot.set_auto_report_state(enable = True)
 # Clear cache sent from the Rosmaster board
 bot.clear_auto_report_data()
 
+ultrasound = Ultrasound()
+event_handler = Event_Handler()
+lidar = Lidar()
+intake = Intake()
+buzzer = Buzzer()
+
 print('Program Start with a sleep of 15 seconds')
-time.sleep(15)
+#time.sleep(15)
 bot.set_beep(100)
-chassis = Chassis()
+chassis = Chassis(ultrasound, lidar, intake, event_handler, buzzer)
 ultrasound = Ultrasound()
 ser = serial.Serial("/dev/ttyACM0", 115200)
 
 print("Start Measure")
-yaw_rate = chassis.measure_stationary_yaw_drift_rate(15)
+yaw_rate = chassis.measure_stationary_yaw_drift_rate(10)
 bot.set_beep(100)
 print("Yaw Rate: {}".format(yaw_rate))
 print('IMU gloabl start: {}'.format(chassis.imu_global_start))
 
-# time.sleep(0.2)
-#chassis.forward()
+chassis.forward()
 #time.sleep(0.2)
-chassis.right()
+#chassis.right()
 #time.sleep(0.2)
 #chassis.back()
 #time.sleep(0.2)
