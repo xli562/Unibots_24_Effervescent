6448b276cdb2b29f301b40a71de8a35fb641fe55
Harry-QY <qy250@cam.ac.uk>
2024-03-22 13:00:38 +0000

    pid tuning

--- motor_drive_modules/Event_Handler.py
+++ motor_drive_modules/Event_Handler.py
@@ -45,18 +45,18 @@ class Event_Handler():
 
 event_handler = Event_Handler()
 
-while True:
-    index_out += 1
-    index_in = 0
-    print('Out Index: {}'.format(index_out))
-    while True:
-        print('In Index: {}'.format(index_in))
-        index_in += 1
-        event_handler.check_restart()
-        if event_handler.reset_flag:
-            event_handler.empty_events()
-            break
-        time.sleep(0.1)
+# while True:
+#     index_out += 1
+#     index_in = 0
+#     print('Out Index: {}'.format(index_out))
+#     while True:
+#         print('In Index: {}'.format(index_in))
+#         index_in += 1
+#         event_handler.check_restart()
+#         if event_handler.reset_flag:
+#             event_handler.empty_events()
+#             break
+#         time.sleep(0.1)
 
         # Do the loop actions
         # Check for events - break
---
+++ motor_drive_modules/pid_tuning.py
@@ -0,0 +1,35 @@
+from RosmasterBoard import Rosmaster
+from ultrasound_via_arduino import Ultrasound
+from Chassis import Chassis
+import time
+import serial
+
+bot = Rosmaster()
+bot.create_receive_threading()
+# Enable auto data sending every 40ms.
+bot.set_auto_report_state(enable = True)
+# Clear cache sent from the Rosmaster board
+bot.clear_auto_report_data()
+
+print('Program Start with a sleep of 15 seconds')
+time.sleep(15)
+bot.set_beep(100)
+chassis = Chassis()
+ultrasound = Ultrasound()
+ser = serial.Serial("/dev/ttyACM0", 115200)
+
+print("Start Measure")
+yaw_rate = chassis.measure_stationary_yaw_drift_rate(15)
+bot.set_beep(100)
+print("Yaw Rate: {}".format(yaw_rate))
+print('IMU gloabl start: {}'.format(chassis.imu_global_start))
+
+# time.sleep(0.2)
+#chassis.forward()
+#time.sleep(0.2)
+chassis.right()
+#time.sleep(0.2)
+#chassis.back()
+#time.sleep(0.2)
+#chassis.left()
+# time.sleep(0.2)
