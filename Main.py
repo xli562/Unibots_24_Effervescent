bbd022878dbba9f36c97c5e5a8e80bc5000acb83
Harry-QY <qy250@cam.ac.uk>
2024-03-22 15:48:51 +0000

    updated main.py and chassis.py

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 from motor_drive_modules.Event_Handler import Event_Handler
-from motor_drive_modules.Chassis import Chassis
-from motor_drive_modules.ultrasound_via_arduino import Ultrasound
+from motor_drive_modules.Chassis import *
 from motor_drive_modules.RosmasterBoard import Rosmaster
 import time
 import serial
@@ -13,12 +12,17 @@ bot.create_receive_threading()
 bot.set_auto_report_state(enable = True)
 bot.clear_auto_report_data()
 
+ultrasound = Ultrasound()
+lidar = Lidar()
+intake = Intake()
+event_handler = Event_Handler()
+
+
 print('START')
 print('Program Start with a sleep of 15 seconds')
 time.sleep(15)
 bot.set_beep(100)
-chassis = Chassis()
-ultrasound = Ultrasound()
+chassis = Chassis(ultrasound, lidar, intake, event_handler)
 ser = serial.Serial("/dev/ttyACM0", 115200)
 
 print("Start Measure")
@@ -27,34 +31,28 @@ bot.set_beep(100)
 print("Yaw Rate: {}".format(yaw_rate))
 print('IMU gloabl start: {}'.format(chassis.imu_global_start))
 
-# pseudo function for getting target
-def get_target():
-    return (-1.23, -3.45, 20) # sx, y, angle
-
-
 # Main Loop
 while True:
     chassis.event_handler.empty_events()
     chassis.event_handler.iteration_start_time = time.time()
 
-    while True:
+    while chassis.event_handler.timeout_flag:
         Chassis.forward()
         if chassis.event_handler.reset_flag and chassis.event_handler.timeout_flag:
             break
-        time.sleep(1)
+        time.sleep(0.5)
         Chassis.right()
         if chassis.event_handler.reset_flag and chassis.event_handler.timeout_flag:
             break
-        time.sleep(1)
-        
-        # ....
-    while chassis.event_handler.timeout_flag:
-        pass
 
+    chassis.revert_orientation()
+        
+        
     # return loop
-    while (abs(get_target[0]) > 45) and (abs(get_target[1]) > 45):
-        chassis.forward()
-        chassis.right()
-        if get_target[2] > 10:
-            chassis.turn(get_target[2])
+    while (abs(chassis.find_base[0] > 45)) or (abs(chassis.find_base[1]) > 45):
+        if chassis.find_base[0] > 45:
+            chassis.forward()
+        if chassis.find_base[1] > 45:
+            chassis.right()
+        
 
--- motor_drive_modules/Chassis.py
+++ motor_drive_modules/Chassis.py
@@ -520,8 +520,130 @@ class Lidar:
         plt.show()
 
 
+NUM_OF_ULTRASOUND_SENSOR = 5
+class Ultrasound:
+    def __init__(self, com_port="/dev/ttyACM0", baud_rate=115200):
+        self.ser = serial.Serial(com_port, baud_rate)
+        self.distances = [0] * NUM_OF_ULTRASOUND_SENSOR  # Right_Bottom, Left, Front, Back, Right_Top
+        self.updated_sensors = [False]*NUM_OF_ULTRASOUND_SENSOR
+        self.updated = False # 用来判断是否所有UltrasoundSensor都更新了最新读数
+        self.obstacle_count = [0] * NUM_OF_ULTRASOUND_SENSOR #代表了连续几次iteration检测到了obstacle，为了避免随机触发
+        self.rugby_count = 0 #同理，代表连续几次检测到了rugby
+        # 当不加入Threading，读取数据时会永远停留在receive_distances的while循环中。故需要加入Thread
+        # 先测试阶段，先不放入Thread
+        # 如果测试没问题，加入Thread不单单要uncomment下面两行，还需要添加别的代码（later）
+        # self.running = False
+        # self.receive_thread = None
+
+    def check_update_status(self):
+        for status in self.updated_sensors:
+            if status == False:
+                return False
+        return True
+
+    def receive_distances(self):
+        self.updated = False
+        self.updated_sensors = [False] * NUM_OF_ULTRASOUND_SENSOR
+        while not(self.updated):
+            data_byte = self.ser.readline().strip()
+            try:
+                data_str = data_byte.decode('utf-8')
+                readings = data_str.split(',')
+                for reading in readings:
+                    if reading.startswith("U"):  # Ultrasonic sensor reading
+                        ultrasonic_distance = int(reading.split(':')[1])
+                        sensor_index = int(reading.split(':')[0][1])
+                        self.update_distance(ultrasonic_distance, sensor_index)
+                        self.updated_sensors[sensor_index] = True
+
+                        if(self.check_update_status()):
+                            self.updated = True
+
+                    # if 'Restart' in readings:
+                    #     ROBOT_RESET()
+
+                # Update Rugby连续几次iteration被检测到了            
+                if (self.distances[4] - self.distances[0]) >= 10:
+                    self.rugby_count += 1
+                else:
+                    self.rugby_count = 0
+            except Exception as e:
+                print(e)
+
+    def update_distance(self, distance, sensor_index):
+        self.distances[sensor_index] = distance
+        if (distance < 30 and distance > 0):
+            self.obstacle_count[sensor_index] += 1
+        else:
+            self.obstacle_count[sensor_index] = 0
+
+    def get_distances(self):
+        if (self.updated):
+            return self.distances
+        else:
+            self.receive_distances()
+
+    @property
+    def rugby_right(self):
+        if (self.updated):
+            return (self.rugby_count >= 5)
+            # return (self.distances[4] - self.distances[0]) >= 5
+            # rugby_left is True when the difference between top and bottom sensor > 5cm
+
+    @property
+    def object_left(self):
+        return (self.obstacle_count[1] >= 2)
+        # return (self.distances[4] < 10 and self.distances[4] > 0)
+        # when object detected within 10cm, object detected
+
+    @property
+    def object_right(self):
+        return (self.obstacle_count[4] >= 2)
+        # return (self.distances[1] < 10 and self.distances[1] > 0)
+        # when object detected within 10cm, object detected
+    
+    @property
+    def object_front(self):
+        return (self.obstacle_count[2] >= 1)
+        # return (self.distances[2] < 10 and self.distances[2] > 0)
+        # when object detected within 10cm, object detected
+    
+    @property
+    def object_back(self):
+        return (self.obstacle_count[3] >= 1)
+        # return (self.distances[3] < 10 and self.distances[3] > 0)
+        # when object detected within 10cm, object detected
+
+    @property
+    def check_obstacle(self):
+        obstacle = []
+        for i in range(1,5):
+            if self.distances[i] < 20 and self.distances[i] > 0:
+                obstacle.append(i)
+        return obstacle
+
+    def comments():
+        # ultrasound = Ultrasound()
+        # while True:
+        #     ultrasound.receive_distances()
+        #     distances = ultrasound.get_distances()  
+        #     print("Distances:", distances)
+        #     print("Obstacles at directions: {}".format(ultrasound.check_obstacle))
+        #     time.sleep(0.1)  
+
+        # # Example usage:
+        # if __name__ == "__main__":
+        #     ultrasound = Ultrasound()  # Create an Ultrasound object
+        #     while True:
+        #         distances = ultrasound.receive_distances()
+        #         distances = ultrasound.get_distances()  
+        #         print("Distances:", distances)
+        #         time.sleep(0.1)  
+        pass
+
+
 class Chassis:
-    def __init__(self) -> None:
+    def __init__(self, ultrasound:Ultrasound, lidar:Lidar, intake:Intake, event_handler:Event_Handler) -> None:
         self.vx = 0
         self.vy = 0
         self.vz = 0
@@ -530,9 +652,10 @@ class Chassis:
         self.time_global_start = time.time()
         self.imu_global_start = bot.get_imu_attitude_data()[2]
         self.turn_num = 0
-        self.intake = Intake()
-        self.ultrasound = Ultrasound()
-        self.event_handler = Event_Handler()
+        self.intake = intake
+        self.ultrasound = ultrasound
+        self.lidar = lidar
+        self.event_handler = event_handler
     
     def measure_stationary_yaw_drift_rate(self, duration, plot=False):
         yaws=[]
@@ -746,7 +869,7 @@ class Chassis:
     def find_base(self):
         # Input: 4 sets of xy coordinates - points
         # Might have to make Lidar an attribute of Chassis Class, and call methods of Lidar
-        points = []
+        points = self.lidar.get_last_arena_vertices()        
         if self.turn % 4 == 0: # Base in Quadrant 3
             for point in points:
                 if point[0] < 0 and point[1] < 0:
@@ -774,128 +897,8 @@ class Chassis:
             for i in range((num-2)%4):
                 self.turn(-90)
                 time.sleep(0.5)
-
-
-NUM_OF_ULTRASOUND_SENSOR = 5
-class Ultrasound:
-    def __init__(self, com_port="/dev/ttyACM0", baud_rate=115200):
-        self.ser = serial.Serial(com_port, baud_rate)
-        self.distances = [0] * NUM_OF_ULTRASOUND_SENSOR  # Right_Bottom, Left, Front, Back, Right_Top
-        self.updated_sensors = [False]*NUM_OF_ULTRASOUND_SENSOR
-        self.updated = False # 用来判断是否所有UltrasoundSensor都更新了最新读数
-        self.obstacle_count = [0] * NUM_OF_ULTRASOUND_SENSOR #代表了连续几次iteration检测到了obstacle，为了避免随机触发
-        self.rugby_count = 0 #同理，代表连续几次检测到了rugby
-        # 当不加入Threading，读取数据时会永远停留在receive_distances的while循环中。故需要加入Thread
-        # 先测试阶段，先不放入Thread
-        # 如果测试没问题，加入Thread不单单要uncomment下面两行，还需要添加别的代码（later）
-        # self.running = False
-        # self.receive_thread = None
-
-    def check_update_status(self):
-        for status in self.updated_sensors:
-            if status == False:
-                return False
-        return True
-
-    def receive_distances(self):
-        self.updated = False
-        self.updated_sensors = [False] * NUM_OF_ULTRASOUND_SENSOR
-        while not(self.updated):
-            data_byte = self.ser.readline().strip()
-            try:
-                data_str = data_byte.decode('utf-8')
-                readings = data_str.split(',')
-                for reading in readings:
-                    if reading.startswith("U"):  # Ultrasonic sensor reading
-                        ultrasonic_distance = int(reading.split(':')[1])
-                        sensor_index = int(reading.split(':')[0][1])
-                        self.update_distance(ultrasonic_distance, sensor_index)
-                        self.updated_sensors[sensor_index] = True
-
-                        if(self.check_update_status()):
-                            self.updated = True
-
-                    # if 'Restart' in readings:
-                    #     ROBOT_RESET()
-
-                # Update Rugby连续几次iteration被检测到了            
-                if (self.distances[4] - self.distances[0]) >= 10:
-                    self.rugby_count += 1
-                else:
-                    self.rugby_count = 0
-            except Exception as e:
-                print(e)
-
-    def update_distance(self, distance, sensor_index):
-        self.distances[sensor_index] = distance
-        if (distance < 30 and distance > 0):
-            self.obstacle_count[sensor_index] += 1
-        else:
-            self.obstacle_count[sensor_index] = 0
-
-    def get_distances(self):
-        if (self.updated):
-            return self.distances
-        else:
-            self.receive_distances()
-
-    @property
-    def rugby_right(self):
-        if (self.updated):
-            return (self.rugby_count >= 5)
-            # return (self.distances[4] - self.distances[0]) >= 5
-            # rugby_left is True when the difference between top and bottom sensor > 5cm
-
-    @property
-    def object_left(self):
-        return (self.obstacle_count[1] >= 2)
-        # return (self.distances[4] < 10 and self.distances[4] > 0)
-        # when object detected within 10cm, object detected
-
-    @property
-    def object_right(self):
-        return (self.obstacle_count[4] >= 2)
-        # return (self.distances[1] < 10 and self.distances[1] > 0)
-        # when object detected within 10cm, object detected
-    
-    @property
-    def object_front(self):
-        return (self.obstacle_count[2] >= 1)
-        # return (self.distances[2] < 10 and self.distances[2] > 0)
-        # when object detected within 10cm, object detected
-    
-    @property
-    def object_back(self):
-        return (self.obstacle_count[3] >= 1)
-        # return (self.distances[3] < 10 and self.distances[3] > 0)
-        # when object detected within 10cm, object detected
-
-    @property
-    def check_obstacle(self):
-        obstacle = []
-        for i in range(1,5):
-            if self.distances[i] < 20 and self.distances[i] > 0:
-                obstacle.append(i)
-        return obstacle
-
-    def comments():
-        # ultrasound = Ultrasound()
-        # while True:
-        #     ultrasound.receive_distances()
-        #     distances = ultrasound.get_distances()  
-        #     print("Distances:", distances)
-        #     print("Obstacles at directions: {}".format(ultrasound.check_obstacle))
-        #     time.sleep(0.1)  
-
-        # # Example usage:
-        # if __name__ == "__main__":
-        #     ultrasound = Ultrasound()  # Create an Ultrasound object
-        #     while True:
-        #         distances = ultrasound.receive_distances()
-        #         distances = ultrasound.get_distances()  
-        #         print("Distances:", distances)
-        #         time.sleep(0.1)  
-        pass
+        shift = lidar.get_last_angle_error()
+        self.turn(-shift)
 
 
 
